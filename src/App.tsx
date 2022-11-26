23f228641d3c2999e078b6036b3f00066517eaab
Людмила Алтухова <luda.tamb@mail.ru>
2022-11-26 22:52:02 +0300

    feat: implement main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-const App = () => {
-  return <></>;
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import MainPage from './pages/MainPage';
+import TodoItemPage from './pages/TodoItemPage';
+import Title from './components/Title';
+
+const App: React.FC = () => {
+  return (
+    <div className='flex'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navigate to='/todos' />} />
+          <Route
+            path='/todos'
+            element={
+              <>
+                <Title page='main' />
+                <MainPage />
+              </>
+            }
+          />
+          <Route
+            path='/todos/:id'
+            element={
+              <>
+                <Title page='todo' />
+                <TodoItemPage />
+              </>
+            }
+          />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
