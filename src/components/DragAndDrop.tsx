a98fc0cada8b527c9ce1e6e3a35519add7a8d2c1
Людмила Алтухова <luda.tamb@mail.ru>
2022-11-27 14:04:24 +0300

    feat: add handle dragging

--- src/components/DragAndDrop.tsx
+++ src/components/DragAndDrop.tsx
@@ -1,14 +1,25 @@
-import { Status } from '../types/types';
-import Todolist from './TodoList';
+import React, { useState } from 'react';
+
+import { Status, Todo } from '../types/types';
+import { data } from '../data';
+import TodoList from './TodoList';
 
 const TodoTable: Status[] = ['Queue', 'Development', 'Done'];
 
-export const DragAndDrop = () => {
+const DragAndDrop: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>(data);
+
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragging = (dragging: boolean): void => setIsDragging(dragging);
+
   return (
-    <>
+    <div className='todos-container'>
       {TodoTable.map((item) => (
-        <Todolist status={item} key={item} />
+        <TodoList status={item} key={item} todos={todos} isDragging={isDragging} handleDragging={handleDragging} />
       ))}
-    </>
+    </div>
   );
 };
+
+export default DragAndDrop;
