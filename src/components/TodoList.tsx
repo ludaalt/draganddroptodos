22813ba52705b1ba64ac42b93d797cfadca1d8e9
Людмила Алтухова <luda.tamb@mail.ru>
2022-11-30 18:50:29 +0300

    feat: implement todo list component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Todo, Status } from '../types/types';
+import { RootState } from '../store/store';
+import TodoItem from './TodoItem';
+import { updateTodo } from '../store/updateTodosSlice';
 
 interface Props {
-  todos: Todo[];
   status: Status;
+  isDragging: boolean;
+  handleDragging: (dragging: boolean) => void;
 }
 
-const TodoList: React.FC<Props> = ({ todos, status }) => {
+const TodoList: React.FC<Props> = ({ status, isDragging, handleDragging }) => {
+  const todos = useSelector((state: RootState) => state.todos.todos);
+  const dispatch = useDispatch();
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>): void => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>): void => {
+    e.preventDefault();
+    const id = +e.dataTransfer.getData('text');
+    dispatch(updateTodo({ id, status }));
+    handleDragging(false);
+  };
+
   return (
-    <div className='todos'>
+    <div className='todos' onDragOver={handleDragOver} onDrop={handleDrop}>
       <header>{status} tasks</header>
-      {/* Todos */}
+      {todos?.map(
+        (todo: Todo) =>
+          status === todo.status && <TodoItem key={todo.id} todo={todo} handleDragging={handleDragging} />,
+      )}
     </div>
   );
 };
