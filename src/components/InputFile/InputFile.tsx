62eef6ddd143d38145b7f8251ed6560d69c6e615
Людмила Алтухова <luda.tamb@mail.ru>
2022-12-02 22:49:42 +0300

    feat: implement upload files

--- src/components/InputFile/InputFile.tsx
+++ src/components/InputFile/InputFile.tsx
@@ -1,41 +1,52 @@
 import React, { useState } from 'react';
 
 import classes from './InputFile.module.scss';
+import { getBase64 } from '../../services/getBase64';
 
 const InputFile: React.FC = () => {
   const [fileCount, setFileCount] = useState(0);
 
   const [files, setFiles] = useState<string[]>([]);
 
-  const handleChangeFile: any = (event: any) => {
+  const handleChangeFile = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    if (!event?.target.files || event.target.files.length === 0) return;
     const file = event.target.files[0];
-    const formData = new FormData();
-    formData.append('file', file);
 
     setFiles((state) => [...state, file.name]);
-
     let newCount = fileCount;
     setFileCount(++newCount);
+
+    getBase64(file).then((base64) => {
+      localStorage.fileBase64 = base64;
+    });
   };
 
   return (
-    <>
-      <input
-        onChange={handleChangeFile}
-        type='file'
-        name='file'
-        id='file'
-        className={classes.fieldFile}
-        multiple
-      />
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div style={{ width: 'calc((100% - 20px) / 2)' }}>
+        <input
+          onChange={handleChangeFile}
+          type='file'
+          name='file'
+          id='file'
+          className={classes.fieldFile}
+          multiple
+        />
 
-      <label htmlFor='file'>
-        <div className={classes.fileFake}>
-          {fileCount > 0 ? `Choosen ${fileCount} files` : 'No file choosed'}
-        </div>
-        <div className={classes.fileButton}>Choose file to upload</div>
-      </label>
-    </>
+        <label htmlFor='file'>
+          <div className={classes.fileFake}>
+            {fileCount > 0 ? `Choosen ${fileCount} files` : 'No file choosed'}
+          </div>
+          <div className={classes.fileButton}>Choose file to upload</div>
+        </label>
+      </div>
+
+      <ul style={{ width: 'calc((100% - 20px) / 2)' }}>
+        {files.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
