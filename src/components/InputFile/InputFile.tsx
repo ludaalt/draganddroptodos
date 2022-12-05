21bfd698e0723ac43b826a604755a82f98132dbd
Людмила Алтухова <luda.tamb@mail.ru>
2022-12-05 11:38:30 +0300

    refactor: correct input file component

--- src/components/InputFile/InputFile.tsx
+++ src/components/InputFile/InputFile.tsx
@@ -3,16 +3,24 @@ import React, { useState } from 'react';
 import classes from './InputFile.module.scss';
 import { getBase64 } from '../../services/getBase64';
 
-const InputFile: React.FC = () => {
+interface Props {
+  updateFiles: (b: any) => void;
+}
+
+const InputFile: React.FC<Props> = ({ updateFiles }) => {
   const [fileCount, setFileCount] = useState(0);
 
-  const [files, setFiles] = useState<string[]>([]);
+  const [files, setFiles] = useState<any>([]);
 
   const handleChangeFile = (event: React.ChangeEvent<HTMLInputElement>): void => {
     if (!event?.target.files || event.target.files.length === 0) return;
-    const file = event.target.files[0];
 
-    setFiles((state) => [...state, file.name]);
+    const file = event.target.files[0];
+    const newFiles = [...files];
+    newFiles.push(file.name);
+    setFiles(newFiles);
+
+    updateFiles(file.name);
     let newCount = fileCount;
     setFileCount(++newCount);
 
@@ -37,12 +45,12 @@ const InputFile: React.FC = () => {
           <div className={classes.fileFake}>
             {fileCount > 0 ? `Choosen ${fileCount} files` : 'No file choosed'}
           </div>
-          <div className={classes.fileButton}>Choose file to upload</div>
+          <div className='button'>Choose file to upload</div>
         </label>
       </div>
 
       <ul style={{ width: 'calc((100% - 20px) / 2)' }}>
-        {files.map((item) => (
+        {files.map((item: any) => (
           <li key={item}>{item}</li>
         ))}
       </ul>
