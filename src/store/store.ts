2901c3856f73da9478a8614af34caeaeee4f633f
Людмила Алтухова <luda.tamb@mail.ru>
2022-12-05 11:47:23 +0300

    refactor: correct store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore, combineReducers } from 'redux';
+import { updateTodosReducer } from './updateTodosReducer';
 
-import todosReducer from './updateTodosSlice';
+function saveToLocalStorage(state: RootState): void {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem('persistantState', serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
 
-const store = configureStore({
-  reducer: {
-    todos: todosReducer,
-  },
+function loadFromLocalStorage(): any {
+  try {
+    const serialisedState = localStorage.getItem('persistantState');
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+}
+
+const rootReducer = combineReducers({
+  todos: updateTodosReducer,
 });
+
+const store = createStore(rootReducer);
+
+// const store = createStore(rootReducer, loadFromLocalStorage());
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export type RootState = ReturnType<typeof store.getState>;
 
