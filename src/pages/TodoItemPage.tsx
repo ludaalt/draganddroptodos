57246d9223098b3530f035144d28c6a80e3daa49
Людмила Алтухова <luda.tamb@mail.ru>
2022-12-05 11:45:46 +0300

    refactor: update page return

--- src/pages/TodoItemPage.tsx
+++ src/pages/TodoItemPage.tsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { Todo } from '../types/types';
 
-import TodoItem from '../components/TodoItem';
+import { getDateDiff } from '../services/getDateDiff';
 
 const TodoItemPage: React.FC = () => {
   const todos = useSelector((state: RootState) => state.todos.todos);
@@ -13,7 +13,37 @@ const TodoItemPage: React.FC = () => {
 
   const todoItem = useMemo(() => todos?.find((item: Todo) => item.id === Number(id)), [todos, id]);
 
-  if (todoItem) return <TodoItem todo={todoItem} />;
+  if (todoItem)
+    return (
+      <div
+        style={{
+          margin: '10px 40px',
+        }}
+      >
+        <h2 style={{ textDecoration: 'underline', marginBottom: '20px' }}>{todoItem.title}</h2>
+        <p style={{ marginBottom: '20px' }}>
+          What needs to be done: <br /> {todoItem.description}
+        </p>
+        <p style={{ marginBottom: '20px' }}>
+          Created at: <br /> {todoItem.createdAt}
+        </p>
+        <p style={{ marginBottom: '20px' }}>
+          Working days: <br /> {todoItem.createdAt && getDateDiff(todoItem.createdAt)}
+        </p>
+        <p style={{ marginBottom: '20px' }}>
+          Done at: <br />
+          {todoItem.doneAt}
+        </p>
+        <p style={{ marginBottom: '20px' }}>
+          Priority: <br /> {todoItem.priority}
+        </p>
+        <p style={{ marginBottom: '20px' }}>
+          Status: <br /> {todoItem.status}
+        </p>
+        {todoItem.files && <p>Files: {todoItem.files}</p>}
+      </div>
+    );
+
   return null;
 };
 
